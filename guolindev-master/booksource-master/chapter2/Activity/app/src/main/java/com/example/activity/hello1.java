0d2489aaf902ff95d79027b20c78b29f8a241a83
Qiu-wen-hui <1694177458@qq.com>
2019-10-09 11:31:01 +0800

    郭霖代码

---
+++ guolindev-master/booksource-master/chapter2/Activity/app/src/main/java/com/example/activity/hello1.java
@@ -0,0 +1,13 @@
+package com.example.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hello1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
